ad2d6538044b494e8b425c2f1ea38306b290ff07
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2023-09-23 13:54:59 +0800

    Added example.

--- h-or-h/main.py
+++ h-or-h/main.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import urllib.request
 import zipfile
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers.legacy import RMSprop
 
 
 # training dataset url
@@ -29,3 +31,61 @@ zip_ref = zipfile.ZipFile(validation_file_name, 'r')
 zip_ref.extractall(validation_dir)
 zip_ref.close()
 
+
+# all images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1/255)
+train_generator = train_datagen.flow_from_directory(
+    training_dir,
+    target_size=(300, 300),
+    class_mode='binary'
+)
+
+# all images will be rescaled by 1./255
+validation_datagen = ImageDataGenerator(rescale=1/255)
+validation_generator = train_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(300, 300),
+    class_mode='binary'
+)
+
+# create the model
+model = tf.keras.models.Sequential([
+    # note the input shape is the desired size of the image 300x300 with 3 bytes color
+    # this is the first convolution
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # the second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # the third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # the fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # the fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('horses') and 1 for the other ('humans')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.summary()
+
+# compile the model
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=RMSprop(learning_rate=0.001),
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    train_generator,
+    epochs=15,
+    validation_data=validation_generator
+)
+
+print(history)
