8dcdf53d5f1384a1e6ac613638ce05375a9a9bd0
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2023-10-06 19:32:34 +0800

    Replaced the hello world.

--- HelloWorld/hello_world.py
+++ HelloWorld/hello_world.py
@@ -1,14 +1,11 @@
+import tensorflow as tf
 import numpy as np
-from tensorflow import keras
 
-l0 = keras.layers.Dense(units=1, input_shape=[1])
-model = keras.Sequential([l0])
+model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
 model.fit(xs, ys, epochs=500)
-
 print(model.predict([10.0]))
-print("Here is what I learned: {}".format(l0.get_weights()))
