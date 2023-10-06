00e6b9f5d4b8a61dc78c6b60e8b98a8c897cfcef
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2023-10-06 23:18:29 +0800

    Fixed.

--- HelloWorld/hello_world.py
+++ HelloWorld/hello_world.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 import numpy as np
 
-model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
+l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
+model = tf.keras.Sequential([l0])
+
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
@@ -9,3 +11,5 @@ ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
 model.fit(xs, ys, epochs=500)
 print(model.predict([10.0]))
+
+print('Here is what I learned: {}'.format(l0.get_weights()))
